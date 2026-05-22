75771006088dc9126cc6468aaa4c659e6ed2f291
pinakoladda <pinakoladda8@gmail.com>
2026-05-22 16:53:25 +0200

    feat: AdoptionBanner update

--- src/features/MainPage/components/AdoptionBanner/AdoptionBanner.tsx
+++ src/features/MainPage/components/AdoptionBanner/AdoptionBanner.tsx
@@ -1,13 +1,21 @@
 import { Button } from '../../../../components/Button'
 import { Typography } from '../../../../shared/components/Typography'
 import { PlayIcon } from '../../../../shared/icons/PlayIcon'
+import rectangleYellowImg from './assets/rectangleYellow.png'
+import rectangleOrangeImg from './assets/rectangleOrange.png'
+import handAndPawImage from './assets/handAndPaw.png'
+
 import styles from './index.module.css'
+import { Paw } from '../../../../shared/icons/Paw'
 
 export const AdoptionBanner = () => {
     return (
         <main className={styles.adoptionBanner}>
             <section className={styles.sloganContainer}>
-                <h1 className={styles.title}>Adoption</h1>
+                <div className={styles.titleContainer}>
+                    <h1 className={styles.title}>Adoption</h1>
+                    <Paw />
+                </div>
                 <Typography
                     className={styles.subtitle}
                     variant="heading2"
@@ -20,8 +28,8 @@ export const AdoptionBanner = () => {
                     variant="body4"
                     weight="medium"
                 >
-                    Adopt a pet and give it a home, <br></br>it will be love you
-                    back unconditionally.
+                    Adopt a pet and give it a home, <br />
+                    it will be love you back unconditionally.
                 </Typography>
                 <section className={styles.btnContainer}>
                     <Button size="L" variant="default">
@@ -37,11 +45,21 @@ export const AdoptionBanner = () => {
                     </Button>
                 </section>
             </section>
-            {/* <img
+            <img
                 className={styles.imgYellowRectange}
-                src={rectangleYellow}
+                src={rectangleYellowImg}
                 alt="rectangle yellow"
-            /> */}
+            />
+            <img
+                className={styles.imgOrangeRectange}
+                src={rectangleOrangeImg}
+                alt="rectangle orange"
+            />
+            <img
+                className={styles.imgMain}
+                src={handAndPawImage}
+                alt="paw in hand"
+            />
         </main>
     )
 }
