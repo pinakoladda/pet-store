84c2e9144e22f1240f29e4aa3d7320e4add7b92c
pinakoladda <pinakoladda8@gmail.com>
2026-06-06 13:36:29 +0200

    feat: add scroll image logic into ImageCarousel component

--- src/features/ProductDetailPage/components/ImagesCarousel/ImagesCarousel.tsx
+++ src/features/ProductDetailPage/components/ImagesCarousel/ImagesCarousel.tsx
@@ -11,34 +11,75 @@ import { useMediaQuery } from '@uidotdev/usehooks'
 import { ArrowLeftSmallDeviceIcon } from '../../../../shared/icons/ArrowLeftSmallDeviceIcon'
 
 import styles from './index.module.css'
+import React from 'react'
 
 const PHOTOS = [img1, img2, img3, img4, img5, img6]
 
 export const ImagesCarousel = () => {
+    const [currentImgIndex, setCurrentImgIndex] = React.useState(0)
     const isSmallDevice = useMediaQuery('only screen and (max-width : 600px)')
+
+    const photosRef = React.useRef<HTMLDivElement>(null)
+
+    const onNextImg = () => {
+        setCurrentImgIndex((value) => {
+            if (value === PHOTOS.length - 1) {
+                return 0
+            }
+            return value + 1
+        })
+    }
+
+    const onBackImg = () => {
+        setCurrentImgIndex((value) => {
+            if (value === 0) {
+                return PHOTOS.length - 1
+            }
+            return value - 1
+        })
+    }
+
+    React.useEffect(() => {
+        const container = photosRef?.current
+        const selectedImg = container?.querySelector(
+            "img[data-selected='true']"
+        )
+        const selectedImgPosition = selectedImg?.getBoundingClientRect().left
+
+        container?.scrollTo({ left: selectedImgPosition, behavior: 'smooth' })
+    }, [currentImgIndex])
 
     return (
         <main className={styles.imagesCarouselSection}>
             <section className={styles.carousel}>
-                <img className={styles.imgMain} src={img1} alt="dog" />
-                <div className={styles.photosContainer}>
-                    {PHOTOS.map((photo) => {
+                <div className={styles.imgContainer}>
+                    <img
+                        className={styles.imgMain}
+                        src={PHOTOS[currentImgIndex]}
+                        alt="dog"
+                    />
+                    <span className={styles.imgArrowRight} onClick={onNextImg}>
+                        <ArrowRightFilledIcon />
+                    </span>
+                    <span className={styles.imgArrowLeft} onClick={onBackImg}>
+                        <ArrowLeftFilledIcon />
+                    </span>
+                </div>
+                <div ref={photosRef} className={styles.photosContainer}>
+                    {PHOTOS.map((photo, index) => {
                         return (
                             <img
+                                data-selected={index === currentImgIndex}
                                 key={photo}
                                 className={styles.imgSecondary}
                                 src={photo}
                                 alt="dog"
+                                onClick={() => setCurrentImgIndex(index)}
                             />
                         )
                     })}
                 </div>
-                <span className={styles.imgArrowRight}>
-                    <ArrowRightFilledIcon />
-                </span>
-                <span className={styles.imgArrowLeft}>
-                    <ArrowLeftFilledIcon />
-                </span>
+
                 {isSmallDevice && (
                     <>
                         <span className={styles.imgInfoSmallDevice}>
